bcefc9c3caf0ec453a45bbe95f776df6384472f4
Morin Sultan <sultanmorin2001@gmail.com>
2025-10-01 19:58:49 +0100

    Update Header component: add dropdown hook and integrate with contect api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,37 +3,138 @@ import styles from "./Header.module.css";
 import logo from "../assets/images/logo.svg";
 import dropdopIcon from "../assets/images/icon-dropdown.svg";
 import unitsIcon from "../assets/images/icon-units.svg";
+import checkIcon from "../assets/images/icon-checkmark.svg";
+
+import { useDropdown } from "../assets/hooks/useDropdown";
+import { useWeatherContext } from "../contexts/WeatherContext";
+
 function Header() {
+  const { open, setOpen, ref } = useDropdown();
+  const {
+    measurementUnit,
+    dispatch,
+    tempUnit,
+    windSpeedUnit,
+    precipitationUnit,
+  } = useWeatherContext();
+
+  function changeUnit() {
+    dispatch({ type: "measurementUnit/change" });
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
       </div>
-      <button className={styles.unitBtn}>
+      <button
+        onClick={() => setOpen((active) => !active)}
+        className={styles.unitBtn}
+      >
         <img src={unitsIcon} alt="units" />
         Units
         <img src={dropdopIcon} alt="dropdown" />
       </button>
 
-      {false && (
-        <div className={styles.unitDropdown}>
-          <button className={styles.toggle}>Switch to Imperial</button>
+      {open && (
+        <div className={styles.unitDropdown} ref={ref}>
+          <button className={styles.toggle} onClick={changeUnit}>
+            {measurementUnit === "metric"
+              ? "Switch to Imperial"
+              : "Switch to Metric"}
+          </button>
+
           <div className={styles.tempUnit}>
             <span>Temperature</span>
-            <button>Celsius (&deg;C)</button>
-            <button>Fahrenheit (&deg;F)</button>
+            <button
+              onClick={() =>
+                dispatch({ type: "tempUnit/change", payload: "C" })
+              }
+              className={`${styles.btnUp} ${
+                tempUnit === "C" ? styles.activeBtn : ""
+              }`}
+            >
+              Celsius (&deg;C)
+              {tempUnit === "C" && <img src={checkIcon} alt="checkmark" />}
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({ type: "tempUnit/change", payload: "F" })
+              }
+              className={`${tempUnit === "F" ? styles.activeBtn : ""}  ${
+                styles.btnDown
+              } `}
+            >
+              Fahrenheit (&deg;F)
+              {tempUnit === "F" && <img src={checkIcon} alt="checkmark" />}
+            </button>
           </div>
+
+          <hr />
 
           <div className={styles.speedUnit}>
             <span>Wind Speed</span>
-            <button>km/h</button>
-            <button>mph</button>
+            <button
+              onClick={() =>
+                dispatch({ type: "windSpeedUnit/change", payload: "kmh" })
+              }
+              className={`${windSpeedUnit === "kmh" ? styles.activeBtn : ""} ${
+                styles.btnUp
+              }`}
+            >
+              km/h
+              {windSpeedUnit === "kmh" && (
+                <img src={checkIcon} alt="checkmark" />
+              )}
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({ type: "windSpeedUnit/change", payload: "mph" })
+              }
+              className={`${windSpeedUnit === "mph" ? styles.activeBtn : ""}  ${
+                styles.btnDown
+              } `}
+            >
+              mph
+              {windSpeedUnit === "mph" && (
+                <img src={checkIcon} alt="checkmark" />
+              )}
+            </button>
           </div>
+
+          <hr />
 
           <div className={styles.precipitationUnit}>
             <span>Precipitation</span>
-            <button> Millimeters (mm)</button>
-            <button> Inches (in)</button>
+            <button
+              onClick={() =>
+                dispatch({ type: "precipitationUnit/change", payload: "mm" })
+              }
+              className={`${
+                precipitationUnit === "mm" ? styles.activeBtn : ""
+              } ${styles.btnUp}`}
+            >
+              Millimeters (mm)
+              {precipitationUnit === "mm" && (
+                <img src={checkIcon} alt="checkmark" />
+              )}
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({ type: "precipitationUnit/change", payload: "in" })
+              }
+              className={`${
+                precipitationUnit === "in" ? styles.activeBtn : ""
+              }  ${styles.btnDown} `}
+            >
+              Inches (in)
+              {precipitationUnit === "in" && (
+                <img src={checkIcon} alt="checkmark" />
+              )}
+            </button>
           </div>
         </div>
       )}
